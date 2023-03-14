df6f5eda89b38b58295feb199937fd0efc5a273e
al-shahzeb <alishahzeb453@gmail.com>
2023-03-15 00:00:56 +0530

    Pizza Billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,13 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+
+        if(isVeg)
+            this.price=300;
+        else
+            this.price=400;
+
+        System.out.println("Base Price Of The Pizza: "+this.price);
     }
 
     public int getPrice(){
@@ -17,18 +24,28 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        System.out.println("Extra Cheese Added: 80");
+        price+=80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        System.out.println("Extra Toppings Added: 70");
+        if(isVeg)
+            this.price+=70;
+        else
+            this.price+=120;
     }
 
     public void addTakeaway(){
         // your code goes here
+        System.out.println("Paperbag Added: 20");
+        price+=20;
     }
 
     public String getBill(){
         // your code goes here
+        bill="Total Price: "+getPrice()+"\n";
         return this.bill;
     }
 }

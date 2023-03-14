fa0bdee16aa9d60cf6654bcc2132a899c8313d82
al-shahzeb <alishahzeb453@gmail.com>
2023-03-15 00:27:52 +0530

    Pizza Billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,14 +9,14 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
-
+        bill="";
         if(isVeg) {
             this.price = 300;
-            System.out.println("Base Price Of The Pizza: 300");
+            bill+="Base Price Of The Pizza: 300\n";
         }
         else {
             this.price = 400;
-            System.out.println("Base Price Of The Pizza: 400");
+            bill+="Base Price Of The Pizza: 400\n";
         }
 
     }
@@ -27,31 +27,32 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        System.out.println("Extra Cheese Added: 80");
-        price+=80;
+        this.bill+="Extra Cheese Added: 80\n";
+        this.price+=80;
     }
 
     public void addExtraToppings(){
         // your code goes here
         if(isVeg) {
             this.price += 70;
-            System.out.println("Extra Toppings Added: 70");
+            bill+="Extra Toppings Added: 70\n";
         }
         else {
-            System.out.println("Extra Toppings Added: 120");
+            bill+="Extra Toppings Added: 120\n";
             this.price += 120;
         }
     }
 
     public void addTakeaway(){
         // your code goes here
-        System.out.println("Paperbag Added: 20");
+        bill+="Paperbag Added: 20\n";
+        //System.out.println();
         price+=20;
     }
 
     public String getBill(){
         // your code goes here
-        bill="Total Price: "+getPrice()+"\n";
+        bill+="Total Price: "+this.price+"\n";
         return this.bill;
     }
 }
